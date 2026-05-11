c097192a63bb338129b86e0328f1ed6bbf1ef01a
Afonso.FL <afonso.f.limaa@gmail.com>
2026-05-11 15:14:28 +0000

    teste

--- galeria/views.py
+++ galeria/views.py
@@ -10,20 +10,24 @@
 from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
+
 def index(request):
-    fotografias = Fotografia.objects
-        .order_by("data_fotografia")
-        .filter(publicada=True)
+    fotografias = Fotografia.objects.filter(
+        publicada=True
+    ).order_by("data_fotografia")
+
     return render(
-        request, 
-        'galeria/index.html', 
+        request,
+        'galeria/index.html',
         {"cards": fotografias}
     )
 
+
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
+
     return render(
-        request, 
-        'galeria/imagem.html', 
+        request,
+        'galeria/imagem.html',
         {"fotografia": fotografia}
     )
